0e53c42a2bd81e0ac80efdfda134a68215f97be3
Latiff <laiffdev@gmail.com>
2022-09-03 14:01:42 +0200

    added new feature
    generate a new password when user clicks on generate button

---
+++ index.js
@@ -0,0 +1,42 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+   "/"];
+
+let passwordOne = document.getElementById("password-one");
+let passwordTwo = document.getElementById("password-two");
+
+
+
+function pass1(length) {
+   for (let i = 0; i < length; i++) {
+      let randomPassword = Math.floor(Math.random() * characters.length);
+      passwordOne.textContent += characters[randomPassword];
+   } 
+}
+
+function pass2(length) {
+   for (let i = 0; i < length; i++){
+      let randomPassword = Math.floor(Math.random() * characters.length);
+      passwordTwo.textContent += characters[randomPassword];
+   }
+}
+
+ 
+
+function generate() {
+   reset();
+   
+   pass1(8);
+   pass2(8);
+}
+
+function reset() {
+   passwordOne.textContent = "";
+   passwordTwo.textContent = "";
+}
+
+
+
+
+
+
+
